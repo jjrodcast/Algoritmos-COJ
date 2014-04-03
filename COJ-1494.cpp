57728636754b4cd85ebae72a9de783de598238dd
JJorgeRCastillo <kanh_2832@hotmail.com>
2014-04-03 12:30:41 -0500

    Create COJ-1494.cpp

---
+++ COJ-1494.cpp
@@ -0,0 +1,10 @@
+#include <cstdlib>
+#include <iostream>
+#include <string.h>
+using namespace std;
+string word;
+int main()
+{   cin>>word;
+    cout<<"I got my first solution!!!"<<endl; 
+    return 0;
+}
