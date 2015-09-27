32412215b2f9d4198a3c332e69c2d7b07d174fc9
JJorgeRCastillo <kanh_2832@hotmail.com>
2015-09-27 17:24:01 -0500

    Solucion del problema COJ-1328.cpp

--- COJ-1328.cpp
+++ COJ-1328.cpp
@@ -1,7 +1,32 @@
-#incude <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	int t;
+	cin >> t;
+	while(t--){
+		double n;
+		int total = 0;
+		int m[51];
+		int p[51];
+		cin >> n;
+		for(int i = 0; i < n; i++){	
+			cin >> m[i]; 
+			total += m[i]; 
+		}
+		for(int i = 0; i <n; i++){
+			cin >> p[i];
+			total += p[i];
+		}
+		total  = total / n;
+		int sol = 0;
+		for(int i = 0; i < n; i++){
+			if(m[i]+p[i] < total) sol++;
+		}
+		cout << sol << "\n";
+	}
     return 0;
 }
